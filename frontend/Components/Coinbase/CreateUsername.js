06942c98a842c4e2eea157100fa0299084789b41
Elizabeth Eidelson <fluturecode@gmail.com>
2022-03-22 22:52:21 -0400

    added vews to CreateUsername

--- frontend/Components/Coinbase/CreateUsername.js
+++ frontend/Components/Coinbase/CreateUsername.js
@@ -1,9 +1,20 @@
+import {SafeAreaView} from 'react-native-';
 import HeaderImage from '../../../assets/image/header1.png';
 
 export const CreateUsername = ({navigation}) => {
   return (
-    <View>
-      <Image src={HeaderImage} />
-    </View>
+    <SafeAreaView>
+      <Image src={HeaderImage} style={{width: '100%'}} />
+      <View>
+        <Text
+          style={{
+            fontFamily: 'Helvetica Neue',
+            fontWeight: '700',
+            fontSize: 24,
+          }}>
+          Pick your username
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 };
